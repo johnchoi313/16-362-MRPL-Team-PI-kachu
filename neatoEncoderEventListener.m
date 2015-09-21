77e9bcf95d120d75d738909b4cf01bdc51725df3
John Choi <johnchoi313@gmail.com>
2015-09-21 19:59:26 -0400

    Works.
    Using time stamped encoder headers with double precision casting.

--- neatoEncoderEventListener.m
+++ neatoEncoderEventListener.m
@@ -1,39 +1,56 @@
 function neatoEncoderEventListener(obj,msg)
-    %globals
+    %% GLOBALS %%
     %global initTime;
     %initTime = tic;
     global currTime;
     global encL;
     global encR;
-    
-    %Get time stamp
+    global V;
+    global T;
+    global INITIAL_TIME;
+   
+    %% Update Previous Time Stamp
     prevTime = currTime;
-    currTime = toc; %- initTime; %obj.LatestMessage.Header.Stamp.Sec; 
-    %+(obj.LatestMessage.Header.Stamp.Nsec/1000000000);
     
-    %Get encoder values
+    %% Get Current Time Stamp
+    %currTime = toc; %- initTime; 
+    currTime = double(obj.LatestMessage.Header.Stamp.Sec) ...
+        + double(obj.LatestMessage.Header.Stamp.Nsec) / 1000000000.0;
+    currTime = currTime - INITIAL_TIME;
+    
+    
+    %% Update Previous Encoder Values
     prevEncL = encL;
     prevEncR = encR;
-    encL = msg.Left;
-    encR = msg.Right;
     
-    %Check if left encoder changed
+    %% Get Current Encoder Values
+    encL = double(msg.Left);
+    encR = double(msg.Right);
+    
+    %Check if Left Encoder Changed
     if(encL ~= prevEncL)
-        %Calculate Ds
+        %Calculate Ds (Encoder Distance)
         dsLeft = encL - prevEncL;
         %Calculate dT
         dt = currTime - prevTime;
         %Calculate V
-        vL = double(dsLeft) / double(dt);
-        
-        % For Plotting
-        %V = [V vL];
-        %T = [T currTime]; 
-        
-        %Plot
-        figure(1);
-        plot(currTime,vL), title('Robot Velocity vs Time'), xlabel('Time'), ylabel('Velocity');
+        %vL = double(dsLeft) / double(dt);
+        vL = dsLeft / dt;
 
+        
+        %% CULL OUT SPIKES
+        %if(vL < 200)
+            %% For Plotting
+            V = [V vL];
+            T = [T currTime];
+            %Plot
+            %figure(1);
+            %plot(currTime,vL), title('Robot Velocity vs Time'), xlabel('Time'), ylabel('Velocity');
+            %plot(T, V), title('Robot Velocity vs Time'), xlabel('Time'), ylabel('Velocity');
+            %line(currTime,vL);
+            %drawnow;
+        %end
+        
     end
         
     %Check if right encoder changed
