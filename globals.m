9c4e4f8c563f3424fcf11c595b1fc7c459e22a64
John Choi <johnchoi313@gmail.com>
2015-09-21 19:02:16 -0400

    Working globals with listener and updating graph
    Lots of spikes, but otherwise working.

--- globals.m
+++ globals.m
@@ -1,13 +1,27 @@
-% --- GLOBALS --- %
+%% --- GLOBALS --- %%
+%% EX2 Case 2
+global V; 
+V = [];  %zeros(1,200);
+global T; 
+T = []; %zeros(1,200);
+
+tic;
+   
+%global myPlot;
+%myPlot = line('xdata', [], 'ydata', []); %plot(T, V, 'b-');
+
 
 %Begin the encoder callback
 robot.encoders.NewMessageFcn=@neatoEncoderEventListener;
 enc=rossubscriber('/enc', @neatoEncoderEventListener);
 
+% Plotting
+%global myPlot;
+
 %Send velocity for a bit
- for i = 1:10
-    robot.sendVelocity(0.03, 0.03);
-    pause(3);
-    robot.sendVelocity(0.0, 0.0);
- end
+for i = 1:10
+    robot.sendVelocity(0.15, 0.15);
+    pause(1);
+end
+robot.sendVelocity(0.0, 0.0);
  
