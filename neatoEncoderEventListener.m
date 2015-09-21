804f69e0671f796197ff3252c2c48c713daa783d
John Choi <johnchoi313@gmail.com>
2015-09-21 19:01:21 -0400

    Working Event listener with updating graph
    Lots of spikes, but approximately correct.

--- neatoEncoderEventListener.m
+++ neatoEncoderEventListener.m
@@ -1,19 +1,44 @@
 function neatoEncoderEventListener(obj,msg)
     %globals
-    global neatoEncoderDataTimeStamp;
-    global neatoEncoderRight;
-    global neatoEncoderLeft;
+    %global initTime;
+    %initTime = tic;
+    global currTime;
+    global encL;
+    global encR;
     
     %Get time stamp
-    neatoEncoderDataTimeStamp=obj.LatestMessage.Header.Stamp.Sec +(obj.LatestMessage.Header.Stamp.Nsec/1000000000);
-      
+    prevTime = currTime;
+    currTime = toc; %- initTime; %obj.LatestMessage.Header.Stamp.Sec; 
+    %+(obj.LatestMessage.Header.Stamp.Nsec/1000000000);
+    
+    %Get encoder values
+    prevEncL = encL;
+    prevEncR = encR;
+    encL = msg.Left;
+    encR = msg.Right;
+    
     %Check if left encoder changed
-    if(neatoEncoderLeft ~= msg.Left)
-        neatoEncoderLeft = msg.Left
+    if(encL ~= prevEncL)
+        %Calculate Ds
+        dsLeft = encL - prevEncL;
+        %Calculate dT
+        dt = currTime - prevTime;
+        %Calculate V
+        vL = double(dsLeft) / double(dt);
+        
+        % For Plotting
+        %V = [V vL];
+        %T = [T currTime]; 
+        
+        %Plot
+        figure(1);
+        plot(currTime,vL), title('Robot Velocity vs Time'), xlabel('Time'), ylabel('Velocity');
+
+    end
+        
+    %Check if right encoder changed
+    if(encR ~= prevEncL)
     end
     
-    %Check if right encoder changed    
-    if(neatoEncoderRight ~= msg.Right)
-        neatoEncoderRight = msg.Right
-    end
 end
+
