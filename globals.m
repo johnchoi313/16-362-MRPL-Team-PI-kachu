b99def14596cc6b32e4bd3c5a4f73342b6bf9ce7
John Choi <johnchoi313@gmail.com>
2015-09-21 19:58:42 -0400

    Works.
    Plotting in real time with double casted time stamped encoder velocities.

--- globals.m
+++ globals.m
@@ -1,9 +1,14 @@
 %% --- GLOBALS --- %%
 %% EX2 Case 2
 global V; 
-V = [];  %zeros(1,200);
+V = []; %zeros(1,500);
 global T; 
-T = []; %zeros(1,200);
+T = []; %zeros(1,500);
+global INITIAL_TIME;
+
+INITIAL_TIME = double(robot.encoders.LatestMessage.Header.Stamp.Sec) ...
++ double(robot.encoders.LatestMessage.Header.Stamp.Nsec) /1000000000.0;
+
 
 tic;
    
@@ -12,16 +17,19 @@ tic;
 
 
 %Begin the encoder callback
-robot.encoders.NewMessageFcn=@neatoEncoderEventListener;
-enc=rossubscriber('/enc', @neatoEncoderEventListener);
+robot.encoders.NewMessageFcn = @neatoEncoderEventListener;
+enc = rossubscriber('/enc', @neatoEncoderEventListener);
 
 % Plotting
 %global myPlot;
 
 %Send velocity for a bit
-for i = 1:10
+for i = 1:100
     robot.sendVelocity(0.15, 0.15);
-    pause(1);
+    pause(0.1);
+    figure(1);
+    plot(T, V), title('Robot Velocity vs Time'), xlabel('Time'), ylabel('Velocity')
+
 end
 robot.sendVelocity(0.0, 0.0);
  
