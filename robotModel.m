dcdd8d1601ce7062ad8369a771b2a3f2c6793843
nikhilmohan33 <nikhilm@andrew.cmu.edu>
2015-10-05 19:29:26 -0400

    Compiles! Uses superclass figure8ReferenceControl. And added .m extension to name.

--- robotModel.m
+++ robotModel.m
@@ -14,11 +14,13 @@ classdef robotModel
             obj.wheelbase = 0.235; % wheel to wheel distance
         end
         
+        % convert wheel velocities to linear/angular velocity
         function [V w] = vlvrToVw(vl, vr)
             V = (vl + vr) / 2;
             w = (vl - vr) / robotModel.wheelbase; 
         end
         
+        % convert linear/angular velocity to wheel velocities
         function [vl vr] = VwTovlvr(V, w)
             vl = V - (robotModel.wheelbase/2)*w;
             vr = V + (robotModel.wheelbase/2)*w;
