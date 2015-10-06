35d89b7c7b61ecbc97912f93c3076c938a209682
nikhilmohan33 <nikhilm@andrew.cmu.edu>
2015-10-05 23:08:42 -0400

    Update robotTrajectory.m

--- robotTrajectory.m
+++ robotTrajectory.m
@@ -2,41 +2,12 @@
 classdef robotTrajectory < figure8ReferenceControl
     
     properties
-        numSamples; % total number of samples
-        index; % current index we are at
-        
-        % state variables
-        totalDistance;
-        totalTime;
-        curX;
-        curY;
-        curTh;
-        
-        % arrays
-        timeSamples; % Time samples        
-        distSamples; % Distance samples WRT time        
-        poseSamples; % Pose list in [x;y;th] WRT time
-                     % x = 1; y = 2; th = 3; for 2D array access
     end
     
     methods(Static = true)
         % initialize robotTrajectory properties
         function obj = robotTrajectory(n)
             obj = obj@figure8ReferenceControl(0.4,0.4,0.1); 
-            
-            % intialize state variables
-            obj.totalTime = obj.t0;
-            obj.totalDistance = 0;
-            obj.curX = 0;
-            obj.curY = 0;
-            obj.curTh = 0;
-            
-            % initialize arrays
-            obj.numSamples = n;
-            obj.timeSamples = zeros(1,n); %time starts at 0  
-            obj.distSamples = zeros(1,n); % distance starts at 0
-            obj.poseSamples = zeros(3,n); % pose starts at global origin 
-                                          % (x = 0,y = 0,th = 0)
         end
                
         function velocity = getVelocity(obj,t)
@@ -50,27 +21,18 @@ classdef robotTrajectory < figure8ReferenceControl
             omega = w;
         end
         
-        function dist = updateDistance(obj,t)
-            % get dt
-            dt = t - obj.totalTime;
+        function dist = updateDistance(obj,t,dt,d)
             % get velocity
             V = obj.getVelocity(obj,t);
-            % update total distance with increase in distance
-            obj.totalDistance = obj.totalDistance + V*dt;
             % return updated distance
-            dist = obj.totalDistance;
-            % add distance to distSamples
-            obj.distSamples(obj.index) = dist;
+            dist = d + V*dt;
         end
       
-        function [x, y, th] = updatePose(obj,t)  
-            % get dt
-            dt = t - obj.totalTime;
-            
+        function [x, y, th] = updatePose(obj,t,dt,X,Y,Th)   
             % get omega
             w = obj.getOmega(obj,t);
             % update theta
-            obj.curTh = obj.curTh + w*dt;
+            th = Th + w*dt;
             
             % get velocity
             V = obj.getVelocity(obj,t);
@@ -78,27 +40,8 @@ classdef robotTrajectory < figure8ReferenceControl
             dx = V*cos(obj.curTh);
             dy = V*sin(obj.curTh);
             % update x and y position
-            obj.curX = obj.curX + dx*dt;
-            obj.curY = obj.curY + dy*dt;
-            
-            %return x, y and theta as pose
-            x = obj.curX;
-            y = obj.curY;
-            th = obj.curTh;
-            % add pose to poseSamples
-            obj.poseSamples(1, obj.index) = x;
-            obj.poseSamples(2, obj.index) = y;
-            obj.poseSamples(3, obj.index) = th;
-        end
-        
-        function updateTime(obj,t)
-            % update the total time elapsed
-            obj.totalTime = t;
-            % add the time to timeSamples
-            obj.timeSamples(obj.index) = t;
-            % increment to the next index
-            obj.index = obj.index + 1;
-        end
-        
+            x = X + dx*dt;
+            y = Y + dy*dt;
+        end 
     end    
 end
