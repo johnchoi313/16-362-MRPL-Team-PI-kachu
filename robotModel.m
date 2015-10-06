2066476051f87f1e8232d1d0b82685ad9e99fcb7
carter-sharer <cjsharer@gmail.com>
2015-10-05 21:35:39 -0400

    Feedforward Works

--- robotModel.m
+++ robotModel.m
@@ -2,6 +2,7 @@
 classdef robotModel
      
     properties
+        maxV; % maximum velocity
         W; % wheel circumference
         W2; % half wheel circumference
         wheelbase; % wheel to wheel distance
@@ -9,22 +10,40 @@ classdef robotModel
     
     methods(Static = true)
         function obj = robotModel()
+            obj.maxV = 0.3; % maximum velocity
             obj.W = 0.23939; % wheel circumference
             obj.W2 = obj.W/2; % half wheel circumference
             obj.wheelbase = 0.235; % wheel to wheel distance
         end
         
         % convert wheel velocities to linear/angular velocity
-        function [V w] = vlvrToVw(vl, vr)
+        function [V, w] = vlvrToVw(obj, vl, vr)
             V = (vl + vr) / 2;
-            w = (vl - vr) / robotModel.wheelbase; 
+            w = (vl - vr) / obj.wheelbase; 
         end
         
         % convert linear/angular velocity to wheel velocities
-        function [vl vr] = VwTovlvr(V, w)
-            vl = V - (robotModel.wheelbase/2)*w;
-            vr = V + (robotModel.wheelbase/2)*w;
+        function [vl, vr] = VwTovlvr(obj, V, w)
+            vl = V - (obj.wheelbase/2)*w;
+            vr = V + (obj.wheelbase/2)*w;
         end
-    
+        
+        % cap vl and vr values
+        function [vl,vr] = capvlvr(obj, VL,VR)
+            if(VL > obj.maxV)
+                VL = obj.maxV;
+            end
+            if(VL < -obj.maxV)
+                VL = -obj.maxV;
+            end
+            if(VR > obj.maxV)
+                VR = obj.maxV;
+            end
+            if(VR < -obj.maxV)
+                VR = -obj.maxV;
+            end
+            vl = VL;
+            vr = VR;            
+        end
     end
 end
