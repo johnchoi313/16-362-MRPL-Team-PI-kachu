d6019e5b9cdfbfc71bb3813ce28ae5f517c93c0a
nikhilmohan33 <nikhilm@andrew.cmu.edu>
2015-10-06 01:04:42 -0400

    Update trajectoryFollower.m

--- trajectoryFollower.m
+++ trajectoryFollower.m
@@ -2,26 +2,34 @@
 classdef trajectoryFollower < robotTrajectory
 
     properties
-        % current state variables
+        %% ------ SIMULATED (theoretical) ------- %%
+        % current state variables (simulation)
         curX;
         curY;
         curTh;
-        curDist;
-        
         % arrays for plotting
         timeSamples;  
-        distSamples; 
         xSamples;
         ySamples;
-        thSamples; 
+        thSamples;
+        
+        %% ------ ACTUAL (real) ------- %%
+        % actual state variables (real)
+        actX;
+        actY;
+        actTh;
+        % actual arrays for plotting
+        actXSamples;
+        actYSamples;
+        actThSamples;
     end
     
     methods(Static = true)
         % this function makes the robot follow the figure 8
         function obj = trajectoryFollower(robot)
-            n = 250; % numSamples
+            % numSamples
+            n = 250; 
             index = 1;
-      
             % getting superclass information
             obj = obj@robotTrajectory(n); 
             
@@ -29,22 +37,32 @@ classdef trajectoryFollower < robotTrajectory
             obj.curX = 0.0;
             obj.curY = 0.0;
             obj.curTh = 0.0;
-            obj.curDist = 0.0;
+            obj.actX = 0.0
+            obj.actY = 0.0
+            obj.actTh = 0.0
             
             % initialize arrays
             obj.timeSamples = zeros(1,n);
-            obj.distSamples = zeros(1,n);
             obj.xSamples = zeros(1,n);
             obj.ySamples = zeros(1,n);
             obj.thSamples = zeros(1,n); 
-            
+            obj.actXSamples = zeros(1,n);
+            obj.actYSamples = zeros(1,n);
+            obj.actThSamples = zeros(1,n); 
+            % initialize encoders
+            encR = 0.0;
+            encL = 0.0;
+            lastEncR = 0.0;
+            lastEncL = 0.0; 
             %initialize time
             tic;
             t = toc;
             lastTime = t;
             dt = t - lastTime; 
             
+            % run the loop
             while(t < obj.tf)           
+                %% ------ SIMULATED (theoretical) ------- %%
                 % get linear and angular velocity
                 [V, w] = obj.computeControl(obj, t);
                 % convert to wheel velocities
@@ -54,27 +72,48 @@ classdef trajectoryFollower < robotTrajectory
                 % send wheel velocities
                 robot.sendVelocity(vl,vr);   
              
-                % update distance
-                obj.curDist = obj.updateDistance(obj, t, dt, obj.curDist);
                 % update pose
-                [obj.curX, obj.curY, obj.curTh] = obj.updatePose(obj, t, dt, obj.curX, obj.curY, obj.curTh);      
-                
+                [obj.curX, obj.curY, obj.curTh] = ...
+                obj.updatePose(obj, t, dt, obj.curX, obj.curY, obj.curTh);
                 %update arrays
                 obj.timeSamples(index) = t;
-                obj.distSamples(index) = obj.curDist;
-                obj.xSamples(index) = obj.curX;
-                obj.ySamples(index) = obj.curY;
-                obj.thSamples(index) = obj.curTh;
+                obj.xSamples(index) = obj.curX*1000;
+                obj.ySamples(index) = obj.curY*1000;
+                obj.thSamples(index) = obj.curTh*1000;
                 
-                %Plot   
+                %% ------ ACTUAL (real) ------- %%
+                %Calculate encoder change (ds)
+                dEncR = (encR - lastEncR);
+                dEncL = (encL - lastEncL);
+                %Calculate actual wheel velocities
+                actVR = (dEncR / dt);
+                actVL = (dEncL / dt);
+                %actV = (actVL + actVR) / 2.0;
+                %actw = (actVL - actVR) / 235.0; 
+                [actV, actw] = obj.vlvrToVw(obj, actVL, actVR);
+                % update pose
+                [obj.actX, obj.actY, obj.actTh] = ...
+                obj.updateActualPose(obj, t, dt, obj.actX, obj.actY, obj.actTh, actV, actw);      
+                %update arrays (converted mm to m)
+                obj.actXSamples(index) = obj.actX;
+                obj.actYSamples(index) = obj.actY;
+                obj.actThSamples(index) = obj.actTh;                
+                
+                %% ------ Plot ACTUAL vs THEORETICAL ------- %%
                 figure(1);
-                subplot(2,2,1), plot(obj.timeSamples(1:index), obj.xSamples(1:index)), title('X vs Time');
-                subplot(2,2,2), plot(obj.timeSamples(1:index), obj.ySamples(1:index)), title('Y vs Time');
-                subplot(2,2,3), plot(obj.timeSamples(1:index), obj.thSamples(1:index)), title('Th vs Time');
-                subplot(2,2,4), plot(obj.xSamples(1:index), obj.ySamples(1:index)), title('X vs Y');
+                subplot(2,2,1), plot(obj.timeSamples(1:index), obj.xSamples(1:index), obj.timeSamples(1:index), obj.actXSamples(1:index)), title('X vs Time');
+                subplot(2,2,2), plot(obj.timeSamples(1:index), obj.ySamples(1:index), obj.timeSamples(1:index), obj.actYSamples(1:index)), title('Y vs Time');
+                subplot(2,2,3), plot(obj.timeSamples(1:index), obj.thSamples(1:index), obj.timeSamples(1:index), obj.actThSamples(1:index)), title('Th vs Time');
+                subplot(2,2,4), plot(obj.xSamples(1:index), obj.ySamples(1:index), obj.actXSamples(1:index), obj.actYSamples(1:index)), title('X vs Y, actX vs actY');
                 
                 % pause 0.01
                 pause(0.01);
+            
+                % update the encoders
+                lastEncR = encR;
+                lastEncL = encL;
+                encR = (robot.encoders.LatestMessage.Right);
+                encL = (robot.encoders.LatestMessage.Left);    
                 
                 % update the last time
                 lastTime = t;
@@ -84,7 +123,6 @@ classdef trajectoryFollower < robotTrajectory
                 dt = t - lastTime; 
                 % increment the index
                 index = index + 1;
-                
             end
             % stop the robot
             robot.sendVelocity(0.0,0.0);
