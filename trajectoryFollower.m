b3887d884721775848d916f88c8e7ac5f7bd2d62
nikhilmohan33 <nikhilm@andrew.cmu.edu>
2015-10-05 20:21:53 -0400

    Create trajectoryFollower.m

---
+++ trajectoryFollower.m
@@ -0,0 +1,19 @@
+%% robotModel > figure8ReferenceControl > robotTrajectory > trajectoryFollower
+classdef trajectoryFollower < robotTrajectory
+
+    properties
+    
+    end
+    
+    methods(Static = true)
+        function obj = trajectoryFollower()
+    
+            obj = obj@robotTrajectory(1000); 
+            
+            
+            
+        end
+    end
+    
+    
+end
