1fcf5322428931d0bdb7921d5aa704238a55b2c3
nikhilmohan33 <nikhilm@andrew.cmu.edu>
2015-10-06 12:16:01 -0400

    Update trajectoryFollower.m
    final

--- trajectoryFollower.m
+++ trajectoryFollower.m
@@ -29,7 +29,7 @@ classdef trajectoryFollower < robotTrajectory
         function obj = trajectoryFollower(robot)
             % numSamples
             n = 250; 
-            index = 1;
+            index = 0;
             % getting superclass information
             obj = obj@robotTrajectory(n); 
             
@@ -37,9 +37,9 @@ classdef trajectoryFollower < robotTrajectory
             obj.curX = 0.0;
             obj.curY = 0.0;
             obj.curTh = 0.0;
-            obj.actX = 0.0
-            obj.actY = 0.0
-            obj.actTh = 0.0
+            obj.actX = 0.0;
+            obj.actY = 0.0;
+            obj.actTh = 0.0;
             
             % initialize arrays
             obj.timeSamples = zeros(1,n);
@@ -50,70 +50,82 @@ classdef trajectoryFollower < robotTrajectory
             obj.actYSamples = zeros(1,n);
             obj.actThSamples = zeros(1,n); 
             % initialize encoders
-            encR = 0.0;
-            encL = 0.0;
+            %encR = 0.0;
+            %encL = 0.0;
+            encR = double(robot.encoders.LatestMessage.Right)*0.001;
+            encL = double(robot.encoders.LatestMessage.Left)*0.001;    
+            
             lastEncR = 0.0;
-            lastEncL = 0.0; 
+            lastEncL = 0.0;
+            %lastEncR = double(robot.encoders.LatestMessage.Right)*0.001;
+            %lastEncL = double(robot.encoders.LatestMessage.Left)*0.001;    
+                
             %initialize time
             tic;
             t = toc;
             lastTime = t;
-            dt = t - lastTime; 
-            
+            dt = 0.01;
             % run the loop
-            while(t < obj.tf)           
-                %% ------ SIMULATED (theoretical) ------- %%
-                % get linear and angular velocity
-                [V, w] = obj.computeControl(obj, t);
-                % convert to wheel velocities
-                [vl, vr] = obj.VwTovlvr(obj, V, w);
-                % cap wheel speeds
-                [vl, vr] = obj.capvlvr(obj, vl,vr);
-                % send wheel velocities
-                robot.sendVelocity(vl,vr);   
-             
-                % update pose
-                [obj.curX, obj.curY, obj.curTh] = ...
-                obj.updatePose(obj, t, dt, obj.curX, obj.curY, obj.curTh);
-                %update arrays
-                obj.timeSamples(index) = t;
-                obj.xSamples(index) = obj.curX*1000;
-                obj.ySamples(index) = obj.curY*1000;
-                obj.thSamples(index) = obj.curTh*1000;
+            while(t < obj.tf)
                 
-                %% ------ ACTUAL (real) ------- %%
-                %Calculate encoder change (ds)
-                dEncR = (encR - lastEncR);
-                dEncL = (encL - lastEncL);
-                %Calculate actual wheel velocities
-                actVR = (dEncR / dt);
-                actVL = (dEncL / dt);
-                %actV = (actVL + actVR) / 2.0;
-                %actw = (actVL - actVR) / 235.0; 
-                [actV, actw] = obj.vlvrToVw(obj, actVL, actVR);
-                % update pose
-                [obj.actX, obj.actY, obj.actTh] = ...
-                obj.updateActualPose(obj, t, dt, obj.actX, obj.actY, obj.actTh, actV, actw);      
-                %update arrays (converted mm to m)
-                obj.actXSamples(index) = obj.actX;
-                obj.actYSamples(index) = obj.actY;
-                obj.actThSamples(index) = obj.actTh;                
-                
-                %% ------ Plot ACTUAL vs THEORETICAL ------- %%
-                figure(1);
-                subplot(2,2,1), plot(obj.timeSamples(1:index), obj.xSamples(1:index), obj.timeSamples(1:index), obj.actXSamples(1:index)), title('X vs Time');
-                subplot(2,2,2), plot(obj.timeSamples(1:index), obj.ySamples(1:index), obj.timeSamples(1:index), obj.actYSamples(1:index)), title('Y vs Time');
-                subplot(2,2,3), plot(obj.timeSamples(1:index), obj.thSamples(1:index), obj.timeSamples(1:index), obj.actThSamples(1:index)), title('Th vs Time');
-                subplot(2,2,4), plot(obj.xSamples(1:index), obj.ySamples(1:index), obj.actXSamples(1:index), obj.actYSamples(1:index)), title('X vs Y, actX vs actY');
-                
+                if(index > 0)
+                    %% ------ SIMULATED (theoretical) ------- %%
+                    % get linear and angular velocity
+                    [V, w] = obj.computeControl(obj, t);
+                    % convert to wheel velocities
+                    [vl, vr] = obj.VwTovlvr(obj, V, w);
+                    % cap wheel speeds
+                    [vl, vr] = obj.capvlvr(obj, vl,vr);
+                    % send wheel velocities
+                    robot.sendVelocity(vl,vr);   
+
+                    % update pose
+                    [obj.curX, obj.curY, obj.curTh] = ...
+                    obj.updatePose(obj, t, dt, obj.curX, obj.curY, obj.curTh);
+                    %update arrays
+                    obj.timeSamples(index) = t;
+                    obj.xSamples(index) = obj.curX;
+                    obj.ySamples(index) = obj.curY;
+                    obj.thSamples(index) = obj.curTh;
+
+                    %% ------ ACTUAL (real) ------- %%
+                    %Calculate encoder change (ds)
+                    dEncR = (encR - lastEncR);
+                    dEncL = (encL - lastEncL);
+                    %Calculate actual wheel velocities
+                    actVR = (dEncR / dt);
+                    actVL = (dEncL / dt);
+                    [actV, actw] = obj.vlvrToVw(obj, actVL, actVR);
+                    % update pose
+                    [obj.actX, obj.actY, obj.actTh] = ...
+                    obj.updateActualPose(obj, t, dt, obj.actX, obj.actY, obj.actTh, actV, actw, obj.curTh);      
+                    %update arrays (converted mm to m)
+                    obj.actXSamples(index) = obj.actX;
+                    obj.actYSamples(index) = obj.actY;
+                    obj.actThSamples(index) = obj.actTh;                
+
+                    %% ------ Plot ACTUAL vs THEORETICAL ------- %%
+                    figure(1);
+                    subplot(2,2,1), plot(obj.timeSamples(1:index), obj.xSamples(1:index), obj.timeSamples(1:index), obj.actXSamples(1:index)), title('X vs Time');
+                    subplot(2,2,2), plot(obj.timeSamples(1:index), obj.ySamples(1:index), obj.timeSamples(1:index), obj.actYSamples(1:index)), title('Y vs Time');
+                    subplot(2,2,3), plot(obj.timeSamples(1:index), obj.thSamples(1:index), obj.timeSamples(1:index), obj.actThSamples(1:index)), title('Th vs Time');
+                    subplot(2,2,4), plot(obj.xSamples(1:index), obj.ySamples(1:index), obj.actXSamples(1:index), obj.actYSamples(1:index)), title('X vs Y, actX vs actY');
+                    
+                    %Plot Error
+                    figure(2);
+                    subplot(2,1,1), plot(obj.timeSamples(1:index), (obj.xSamples(1:index) - obj.actXSamples(1:index))*100), title('X error');
+                    subplot(2,1,2), plot(obj.timeSamples(1:index), (obj.ySamples(1:index) - obj.actYSamples(1:index))*100), title('Y error');
+                    
+                end
+                    
                 % pause 0.01
                 pause(0.01);
             
                 % update the encoders
                 lastEncR = encR;
                 lastEncL = encL;
-                encR = (robot.encoders.LatestMessage.Right);
-                encL = (robot.encoders.LatestMessage.Left);    
+                encR = double(robot.encoders.LatestMessage.Right)*0.001;
+                encL = double(robot.encoders.LatestMessage.Left)*0.001;    
                 
                 % update the last time
                 lastTime = t;
@@ -124,6 +136,7 @@ classdef trajectoryFollower < robotTrajectory
                 % increment the index
                 index = index + 1;
             end
+            
             % stop the robot
             robot.sendVelocity(0.0,0.0);
             obj.xSamples;
